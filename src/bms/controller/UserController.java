81b0ef74688881668bc8b7856b8095968a0fda91
rishinduyohan <rishinduyohan@gmail.com>
2025-09-02 15:48:23 +0530

    Upadate user controller

--- src/bms/controller/UserController.java
+++ src/bms/controller/UserController.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package bms.controller;
+import bms.DBConnection.DBConnection;
+import java.sql.*;
+import bms.model.User;
 
-/**
- *
- * @author acer
- */
 public class UserController {
-    
+    public static boolean addUser(User user) throws ClassNotFoundException, SQLException{
+        String SQL = "Insert into User values (?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement stm = DBConnection.getInstance().getConnection().prepareStatement(SQL);
+        stm.setObject(1, user.getName());
+        stm.setObject(2, user.getFname());
+        stm.setObject(3, user.getDob());
+        stm.setObject(4, user.getGender());
+        stm.setObject(5, user.getEmail());
+        stm.setObject(6, user.getStatus());
+        stm.setObject(7, user.getAddress());
+        stm.setObject(8, user.getCity());
+        stm.setObject(9, user.getState());
+        stm.setObject(10, user.getPhone());
+        int res = stm.executeUpdate();
+        return res>0;
+    }
 }
